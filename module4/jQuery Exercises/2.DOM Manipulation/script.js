61798dcbd7beb4f6997bf8e119b1bde232b78b93
stratcom <mhstratton@gmail.com>
2014-05-05 21:24:39 -0600

    done with all the list items the same

--- module4/jQuery Exercises/2.DOM Manipulation/script.js
+++ module4/jQuery Exercises/2.DOM Manipulation/script.js
@@ -1,14 +1,21 @@
 
 $(document) .ready(function() {
 	$(".instructions").css({
+		"border": "2px dotted #FFFF00",
 		"color":"#FF3300",
 		"background": "rgba(34,138,34,0.2)",
+		
 	});
 
+
+
 $(".sandbox").css({
+		"border": "3px solid red",
 		"color":"#FFFF00",
 		"background": "rgba(0,0,153,0.2)",
 	});
+
+	$(".sandbox li").text("Four score and seven years ago ...")
 
 })
 
