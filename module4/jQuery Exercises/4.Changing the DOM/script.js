55844459ebcb2ef82e6dac3532bb3853ee01fece
stratcom <mhstratton@gmail.com>
2014-05-06 21:33:58 -0600

    all of Module 4 completed

--- module4/jQuery Exercises/4.Changing the DOM/script.js
+++ module4/jQuery Exercises/4.Changing the DOM/script.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 
-	$(".removeMe").empty() ;
-
+	$(".removeMe p").empty();
 	console.log($("Remove text from removeMe"));
+
+	$(".removeMe").empty();
+	console.log($("Remove paragraph from removeMe"));
+
+	$(".sandbox div").remove();
+	console.log($("Remove div from sandbox"));
+
+	$(".sandbox").append("<div class='target'></div>");
+	console.log($("Add div called target to sandbox"));
+
+	$(".target").append("<p class='newP'></p>");
+	console.log($("Add div called target to sandbox"));
+
+	$(".newP").append("Get your stinking paws off me, you damned dirty ape.");
+	console.log($("Add text to newP"));
 
 
 })
--- module4/jQuery Exercises/5.Selectors/script.js
+++ module4/jQuery Exercises/5.Selectors/script.js
@@ -1,1 +1,23 @@
+$(document) .ready(function() {
+	$(".sandbox p:nth-child(2)").css({
+		"color":"#FF3300",
+	});
+	console.log($("Changed text color in paragraph 2"));
 
+	$(".sandbox p:nth-child(1)").css({
+		"background": "rgba(34,138,34,0.5)",
+	});
+	console.log($("Changed background color in paragraph 1"));
+
+	$(".sandbox p:nth-child(3)").empty()
+	console.log($("Removed text of paragraph 3"));
+
+	$(".sandbox li:nth-child(4)").css({
+		"color":"#6600CC",
+	});
+	console.log($("Change the text color of the fourth list item"));
+
+	$(".sandbox li:nth-child(2)").text("I did it!")
+	console.log($("Change the text of the second list item"));
+
+})
