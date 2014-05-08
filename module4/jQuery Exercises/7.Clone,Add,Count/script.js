03719f90e13a5a7fa376a607cf03492330b8f714
stratcom <mhstratton@gmail.com>
2014-05-07 19:03:49 -0600

    Started Module 7

---
+++ module4/jQuery Exercises/7.Clone,Add,Count/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+
+
+
+//capture the value of the selector
+
+
+
+
+var x= $(.numberToAdd option value=  )
+
+
+// Clone the 'li' item in the template and insert text into it.
+
+
+
+
+
+ 
+
+
+
+
+
+ // Append the cloned element to the list.
+ 
+
+
+
+
+ 
+
+
+
+
+ // Update the counter at the bottom with the number of items 
+ //that have been added by incrementing its' value.
+
+
+
+
+
+})
